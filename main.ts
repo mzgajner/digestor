d0fcd9a9cefbab0c6eb9c7958c41c57c366fb247
mzgajner <mato@zgajner.com>
2023-09-29 16:12:59 +0200

    Respond to HEAD requests

--- main.ts
+++ main.ts
@@ -10,34 +10,40 @@ const port = Number(env["PORT"]) ?? 80;
 async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
 
-  if (request.method === "GET") {
+  if (request.method === "HEAD") {
+    return new Response(null, {
+      status: Status.OK,
+      headers: {
+        "Accept-Ranges": "bytes",
+      },
+    });
+  } else if (request.method === "GET") {
     if (url.pathname === "/podcast/feed.xml") {
-      return await serveFeed()
+      return await serveFeed();
     } else if (url.pathname === "/logo.png") {
-      return await serveLogo()
+      return await serveLogo();
     }
   }
 
   return new Response(null, { status: Status.NotFound });
-};
+}
 
 async function serveFeed() {
   const rawEntries = await fetchFeed();
   const parsedEntries = parseEntries(rawEntries);
-  const feed = generateFeed(parsedEntries)
-  const headers = { 'content-type': 'application/rss+xml' }
-  const status = Status.OK
+  const feed = generateFeed(parsedEntries);
+  const headers = { "content-type": "application/rss+xml" };
+  const status = Status.OK;
 
   return new Response(feed, { headers, status });
 }
 
 async function serveLogo() {
-  const image = await Deno.readFile('./logo.png');
-  const headers = { 'content-type': 'image/png' }
-  const status = Status.OK
+  const image = await Deno.readFile("./logo.png");
+  const headers = { "content-type": "image/png" };
+  const status = Status.OK;
 
-  return new Response(image, { headers, status })
-
+  return new Response(image, { headers, status });
 }
 
 Deno.serve({ port }, handler);
