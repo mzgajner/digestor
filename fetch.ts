b735b8fa542c0d08f74c1ed57f3ed63c6adae81d
mzgajner <mato@zgajner.com>
2023-10-02 00:01:55 +0200

    Fetch real or fake data depending on environment

--- fetch.ts
+++ fetch.ts
@@ -1,3 +1,4 @@
+import { load } from "https://deno.land/std/dotenv/mod.ts";
 import { ExtendedFeedEntry, getEntriesFromFeed } from "./parse.ts";
 import { testNewsXml, testPodcastXml } from "./test-xml.ts";
 
@@ -5,26 +6,31 @@ const NEWS_FEED_URL = "https://radiostudent.si/taxonomy/term/55167/%2A/feed";
 const PODCAST_FEED_URL =
   "https://radiostudent.si/kultura/pritiskavec-gold/podcast";
 
+const env = await load();
+
 export async function fetchFeed() {
-  const newsEntries = [];
-  let entriesInLastRequest = 0;
-  let currentPage = 0;
+  let newsEntries, podcastEntries;
+  if (env["ENVIRONMENT"] === "development") {
+    newsEntries = await getEntriesFromFeed(testNewsXml);
+    podcastEntries = await getEntriesFromFeed(testPodcastXml);
+  } else {
+    newsEntries = [];
+    let entriesInLastRequest = 0;
+    let currentPage = 0;
 
-  do {
-    const entries = await fetchEntries(`${NEWS_FEED_URL}?page=${currentPage}`);
-    newsEntries.push(...entries);
-    entriesInLastRequest = entries.length;
-    currentPage++;
-  } while (entriesInLastRequest > 0);
+    do {
+      const entries = await fetchEntries(
+        `${NEWS_FEED_URL}?page=${currentPage}`,
+      );
+      newsEntries.push(...entries);
+      entriesInLastRequest = entries.length;
+      currentPage++;
+    } while (entriesInLastRequest > 0);
 
-  const podcastEntries = await fetchEntries(PODCAST_FEED_URL);
+    podcastEntries = await fetchEntries(PODCAST_FEED_URL);
+  }
 
-  // const newsEntries = await getEntriesFromFeed(testNewsXml);
-  // const podcastEntries = await getEntriesFromFeed(
-  //   testPodcastXml,
-  // ) as ExtendedFeedEntry[];
-
-  return { newsEntries, podcastEntries };
+  return { newsEntries, podcastEntries: podcastEntries as ExtendedFeedEntry[] };
 }
 
 async function fetchEntries(url: string) {
