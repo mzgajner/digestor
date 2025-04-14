778f1c42913f8a33a962ba81fca6ce6b708232b6
mzgajner <mato@zgajner.com>
2025-04-14 09:55:20 +0200

    Remove 6 episodes Spotify complained about

--- fetch.ts
+++ fetch.ts
@@ -13,6 +13,17 @@ const env = await load()
 const PODCAST_FEED_URL =
   'https://radiostudent.si/kultura/pritiskavec-gold/podcast'
 
+// Spotify complained these contain copyrighted material, so we're excluding
+// them from the feed to avoid any issues.
+const SPOTIFY_BLACKLIST = [
+  'Skupaj je lažje',
+  'Mrknil je Telltale',
+  'Portal',
+  'Jagodni izbor sezone 2017/18',
+  'Iz Rusije z ljubeznijo',
+  'Slišati igro',
+]
+
 export default async function fetchPodcastEntries() {
   // Don't actually fetch, just return test data if in development
   if (env['ENVIRONMENT'] === 'development') {
@@ -33,7 +44,9 @@ export default async function fetchPodcastEntries() {
     currentPage++
   }
 
-  return entries
+  return entries.filter((entry) =>
+    !SPOTIFY_BLACKLIST.includes(entry.title?.value ?? '')
+  )
 }
 
 async function getFeedFromUrl(url: string) {
